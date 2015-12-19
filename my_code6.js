fc4fb9a68093ca4df506c077e920c8a8224beedf
Kent Gray <aggie12man@gmail.com>
2015-12-19 12:38:21 -0800

    JQuery CSS Manipulation
    Little Web Hut JQuery and JS Tutorial 6

---
+++ my_code6.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("h1").mouseenter(function() {
+    $(this).toggleClass();
+      
+      
+      //$(this).removeClass("emphasis").addClass("shrink");
+      
+      
+      //$(this).addClass("emphasis shrink"); 
+      
+      
+      /*$(this).css({
+        "margin-left": "+=50px",
+        "background-color": "red"});*/         
+    
+  });
+});
