44f75783c5f5cb19d37382427f35e2706be43e6a
Kent Gray <aggie12man@gmail.com>
2015-12-19 14:59:16 -0800

    JQuery Variables
    Little Web Hut Tutorial 9 for JS and JQuery

---
+++ my_code9.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    var imageName = ["floatingball.gif", "redball.gif", "eightball.gif"];
+    var indexNum = 0;
+//    var lineNum = 0;
+//    var aNumber = 5;
+//    lineNum = aNumber - 4;
+    
+    
+      
+//    $("#picture").click(function() {
+//        $("#picture").attr("src", imageName[indexNum]);
+//        indexNum++;
+//        if (indexNum > 2) {indexNum = 0;}
+        
+        $("#picture").click(function() {
+
+            $("#picture").fadeOut(1000, function() {
+                $("#picture").attr("src", imageName[indexNum]);
+                indexNum++;	
+                if (indexNum > 2) {indexNum = 0;}
+                $("#picture").fadeIn(1000);                
+            });
+            
+              
+//        $("p").css("background-color", "yellow");
+//        $("p").eq(lineNum).css("background-color", "red");
+//        lineNum++;
+//        if (lineNum > 2) {lineNum = 0;}
+        
+    
+//       var hText = "The heading text is " + $("#head1").text();
+//       var text1 = "The heading text is "+;
+//       var text2 = text1 + hText;
+
+     
+//    $("p").text(hText);
+  });
+});
