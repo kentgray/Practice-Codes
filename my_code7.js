52b843f2d5977b9feebcf85e8d04c759667ce12c
Kent Gray <aggie12man@gmail.com>
2015-12-19 13:13:54 -0800

    JQuery HTML Manipulation
    Little Web Hut JS and JQuery Tutorial 7

---
+++ my_code7.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+//      $("div").html('bam bam <b style="color: red;">Bigelow</b>');
+//      $("div").empty();
+//      $("p").append(" More Words.");
+//      $("div").append("<p>new text</p>");
+//      $("div").after("<p>new text</p>");
+//      $("p").append("<p>new text</p>");
+//      $("div").prepend("<p>New Awesomeness</p>");    
+//      $("div").before("<p>New Awesomeness</p>"); 
+//      $("p").replaceWith("<h2>New Awesomeness</h2>");
+        $("#picture").attr("src", "floatingball.gif");
+//            .attr("src", "eightball.gif"); 
+  });
+}); 
