878d07415c068e1f30c693db69a59bcae488f2db
Kent Gray <aggie12man@gmail.com>
2015-12-19 17:51:31 -0800

    Here is the JS for the HTML on the Sample Game
    JS and JQuery

---
+++ Game Code.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    var imageName = ["floatingball.gif", "redball.gif", "eightball.gif"];
+    var indexNum = 0;
+//    var lineNum = 0;
+//    var aNumber = 5;
+//    lineNum = aNumber - 4;
+    
+    
+      
+//    $("#picture").click(function() {
+//        $("#picture").attr("src", imageName[indexNum]);
+//        indexNum++;
+//        if (indexNum > 2) {indexNum = 0;}
+        
+        $("#picture").click(function() {
+
+            $("#picture").fadeOut(1000, function() {
+                $("#picture").attr("src", imageName[indexNum]);
+                indexNum++;	
+                if (indexNum > 2) {indexNum = 0;}
+                $("#picture").fadeIn(1000);                
+            });
+            $("#btn1").click(function() {
+               $("#picture").animate({height: "300px"});                
+            });
+            $("#btn2").click(function() {
+               $("#picture").animate({height: "100px"});                
+            });
+            
+              
+//        $("p").css("background-color", "yellow");
+//        $("p").eq(lineNum).css("background-color", "red");
+//        lineNum++;
+//        if (lineNum > 2) {lineNum = 0;}
+        
+    
+//       var hText = "The heading text is " + $("#head1").text();
+//       var text1 = "The heading text is "+;
+//       var text2 = text1 + hText;
+
+     
+//    $("p").text(hText);
+  });
+});
