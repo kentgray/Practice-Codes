8f0ae0b869321c2d745e404e714d07cab8ec4aeb
Kent Gray <aggie12man@gmail.com>
2015-12-23 13:07:30 -0800

    Test Application in C#
    Basic App Created from Coursework

---
+++ C#/TestApp.cs
@@ -0,0 +1,15 @@
+//A simple C# Application.
+
+using System.Windows.Forms;
+
+class TestApp
+{
+    static void Main()
+    {
+     Console.WriteLine("Testing! 1, 2, 3");
+        
+        MessageBox.Show("Hello...");
+        
+    }
+}
+
