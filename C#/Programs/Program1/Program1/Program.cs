245de875e2b02d7dbdb2051e7fd10097e2a30e7e
Kent Gray <aggie12man@gmail.com>
2015-12-23 13:32:45 -0800

    C# Apps
    New Folder with New C# Programs

---
+++ C#/Programs/Program1/Program1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name, please:");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+}
