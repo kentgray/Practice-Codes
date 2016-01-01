d0bf84d34523ea58a85bbdda6507d69502ae5cfc
Kent Gray <aggie12man@gmail.com>
2015-12-31 16:34:47 -0800

    C# Casting
    C# Tutorial

--- C#/Programs/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ C#/Programs/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,29 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("5 + 3 = " + (5 + 3));
+            double number1 = 10.5;
+            double number2 = 15;
+
+            Random rand = new Random();
+            Console.WriteLine("Random Number between 1 and 10 " + (rand.Next(1, 11)));
+
+
+            /*
+            Console.WriteLine("Math.Abs(number1) " + (Math.Abs(number1)));
+            Console.WriteLine("Math.Ceiling(number1) " + (Math.Ceiling(number1)));
+            Console.WriteLine("Math.Floor(number1) " + (Math.Floor(number1)));
+            Console.WriteLine("Math.Max(number1, number2) " + (Math.Max(number1, number2)));
+            Console.WriteLine("Math.Min(number1, number2) " + (Math.Min(number1, number2)));
+            Console.WriteLine("Math.Pow(number1, 2) " + (Math.Pow(number1, 2)));
+            Console.WriteLine("Math.Round(number1) " + (Math.Round(number1)));
+            Console.WriteLine("Math.Sqrt(number1) " + (Math.Sqrt(number1)));
+
+          
+
+          /*  double pi = 3.14
+            int intPi = (int)pi;
+
+           /* Console.WriteLine("5 + 3 = " + (5 + 3));
             Console.WriteLine("5 - 3 = " + (5 - 3));
             Console.WriteLine("5 * 3 = " + (5 * 3));
             Console.WriteLine("5 / 3 = " + (5 / 3));
