09da4475811379a007ce570166aac460c91bbbc9
Kent Gray <aggie12man@gmail.com>
2015-12-31 16:14:22 -0800

    C# Data Types
    C# Tutorial

--- C#/Programs/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ C#/Programs/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,9 +11,35 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("What is your name");
+            bool canVote = true;
+
+            char grade = 'A';
+
+            // Integer with a max number of 2,147,483,647
+            int maxInt = int.MaxValue;
+
+            // Long with a max number of 9,223,372,036,854,775,807
+            long maxLong = long.MaxValue;
+
+            // Decimal has a maximum value of 79,228,162,514,264,337,593,543,950,335
+            // If you need something bigger look up BigInteger
+            decimal maxDec = decimal.MaxValue;
+
+            // A float is a 32 bit number with a maxValue of 3.402823E+38 with 7 decimals of precision
+            float maxFloat = float.MaxValue;
+
+            // A float is a 32 bit number with a maxValue of 1.797693134E+308 with 15 decimals of precision
+            double maxDouble = double.MaxValue;
+
+            Console.WriteLine("Max Int: " + maxInt);
+
+            var anotherName = "Tom";
+
+            Console.WriteLine("anotherName is a {0}", anotherName.GetTypeCode());
+
+     /*       Console.WriteLine("What is your name");
             string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Hello " + name); */
         }
     }
 }
