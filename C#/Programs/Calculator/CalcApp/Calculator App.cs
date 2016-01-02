4f5004671a82037411ccb5d9ebd14859908fbcb9
Kent Gray <aggie12man@gmail.com>
2016-01-02 15:08:28 -0800

    Update to Calculator App
    C# Succinctly

--- C#/Programs/Calculator/CalcApp/Calculator App.cs
+++ C#/Programs/Calculator/CalcApp/Calculator App.cs
@@ -1,68 +1,24 @@
-using System;
+﻿using System;
 using System.Text;
 
-// Title: Calculator
-// By: Kent Gray
 
 class Calculator
 {
-    /// This is the entry point.
+
     static void Main()
     {
-        char firstChar = 'Q';
-        bool keepRunning = true;
-    
-        do    
-        {
-        Console.WriteLine();
-        Consol.Write("What do you want to do? (Add, Subtract, Multiply, Divide, Quit): ");
-        string input = Console.Readline();
-        firstChar = input[0];
-        
-        //this is used in both the if statement and the do-while loop.
-        keepRunning = !(firstChar == 'q' || first Char == 'Q');
-        
-        double firstNumber = 0;
-        double secondNumber = 0;
-        
-        if (keepRunning)
-        {
-            Console.Write("First Number: ");
-            string firstNumberInput = Console.Readline();
-            firstNumber = double.Parse(firstNumberInput);
-            
-            Console.Write("Second Number: ");
-            string secondNumberInput = Console.Readline();
-            secondNumber = double.Parse(secondNumberInput);
-        }
-        
-        double result = 0;
-        switch (firstChar)
-        {
-            case 'a':
-            case 'A':
-                result = firstnNumber + secondNumber;
-                break;
-            case 's':
-            case 'S':
-                result = firstNumber - +secondNumber;
-                break;
-            case 'm':
-            case 'M':
-                result = firstnNumber * secondNumber;
-                break;
-            case 'd':
-            case 'D':
-                result = firstNumber / secondNumber;
-                break;
-            default:
-                result = -1;
-                break;
-        }
-        
-        Console.WriteLine();
-        Console.WriteLine("Your result is " + result);
-    
-    } while (keepRunning);
-  }
+        Console.Write("First Number: ");
+        string firstNumberInput = Console.ReadLine();
+        double firstNumber = double.Parse(firstNumberInput);
+
+        Console.Write("Second Number: ");
+        string secondNumberInput = Console.ReadLine();
+        double secondNumber = double.Parse(secondNumberInput);
+
+        double result = firstNumber + secondNumber;
+
+        Console.WriteLine($"\n\tYour result is {result}.");
+
+        Console.ReadKey();
+    }
 }
