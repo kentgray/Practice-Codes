b982e152aeba46ec535e333556ba2d88428013e6
Kent Gray <aggie12man@gmail.com>
2016-01-06 11:23:14 -0800

    Sample code in C#
    C# Succinctly Tutorial

---
+++ C#/Programs/Calculator1/Calculator1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        var calc4 = new Calculator4();
+        calc4.GetNumber("First");
+        calc4.GetNumber("Second");
+        calc4.AddNumbers();
+        PrintResult(calc4);
+        Console.ReadKey();
+    }
+    static void PrintResult(Calculator4 calc)
+    {
+        Console.WriteLine($"Your result is {result}.");
+    }
+}
