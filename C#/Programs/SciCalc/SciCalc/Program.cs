ae09161b9517a58a9470161e2494efc03051ea2b
Kent Gray <aggie12man@gmail.com>
2016-01-06 13:08:10 -0800

    C# Scientific Calculator
    C# Succinctly Tutorial

---
+++ C#/Programs/SciCalc/SciCalc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        ScientificCalculator sciCalc = new ScientificCalculator();
+        double powerResult = sciCalc.Power(2, 5);
+        Console.WriteLine($"Scientific Calculator 2**5: {powerResult}");
+        double sciSum = sciCalc.Add(3, 3);
+        Console.WriteLine($"Scientific Calculator 3 + 3: {sciSum}");
+        ProgrammerCalculator prgCalc = new ProgrammerCalculator();
+        double orResult = prgCalc.Or(5, 10);
+        Console.WriteLine($"Programmer Calculator 5 | 10: {orResult}");
+        44
+    double prgSum = prgCalc.Add(3, 3);
+        Console.WriteLine($"Programmer Calculator 3 + 3: {prgSum}");
+        Console.ReadKey();
+    }
+}
