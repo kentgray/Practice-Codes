87eab0388c88cca079f70a2edbced32fa7b0c033
Kent Gray <aggie12man@gmail.com>
2016-01-06 11:24:25 -0800

    C# Code using Exception Handling
    C# Succinctly

---
+++ C#/Programs/Calculator1/Calculator1/Class2.cs
@@ -0,0 +1,14 @@
+﻿static void HandleNullReference()
+{
+    Program prog = null;
+    try
+    {
+        Console.WriteLine(prog.ToString());
+        38
+    }
+    catch (NullReferenceException ex)
+    {
+        Console.WriteLine(ex.Message);
+
+    }
+}
