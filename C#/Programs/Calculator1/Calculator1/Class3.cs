c1a09f194f0b6af2adb7b252b4bb068950ae1a7c
Kent Gray <aggie12man@gmail.com>
2016-01-06 11:26:15 -0800

    C# Exception Handling with multiple catch blocks
    C# Succinctly Tutorial

---
+++ C#/Programs/Calculator1/Calculator1/Class3.cs
@@ -0,0 +1,24 @@
+﻿static void HandleUncaughtException()
+{
+    Program prog = null;
+    try
+    {
+        Console.WriteLine(prog.ToString());
+    }
+    catch (ArgumentNullException ex)
+    {
+        Console.WriteLine("From ArgumentNullException: " + ex.Message);
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine("From ArgumentException: " + ex.Message);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("From Exception: " + ex.Message);
+    }
+    finally
+    {
+        Console.WriteLine("Finally always executes.");
+    }
+}
