63d93135662a7208e8136088bd11f41585cd0891
Kent Gray <aggie12man@gmail.com>
2016-01-07 11:13:09 -0800

    Operator Overloading in C#
    C# Succinctly Tutorial

---
+++ C#/Programs/Succinctly Codes/Operator Overlaoading.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+
+namespace ConsoleApplication1
+{
+    class Animal
+    {
+        public double height { get; set; } 
+        public double weight { get; set; }
+        public string sound { get; set; }
+
+        public string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+
+        }
+
+        publiAnimal()
+        {
+            this.height = 0;
+            this.weight = 0;
+            this.name = "No Name";
+            this.sound = "No Sound";
+        }
+
+        publicAnimal(double height, double weight, string name, string soun d)
+        {
+            this.height = height;
+            this.weight = weight;
+            this.name = name;
+            this.sound = sound;
+            numOfAnimals++;
+        }
+
+        static int numOfAnimals = 0;
+
+        public static int getNumOfAnimals()
+        {
+            return numOfAnimals;
+        }
+
+        public string toString()
+        {
+            return String.Format("{0} is {1} inches tall, weighs {2} lbs and likes to say {3}", name, height, weight, sound);
+        }
+
+        public int getSum(int num1 = 1, int num2 = 1)
+        {
+            return num1 + num2;
+        }
+
+        public double getSum(double num1 = 1, double num2 = 1)
+        {
+            return num1 + num2;
+        }
+
+        static void Main(string[] args)
+        {
+            Shape rect = new Rectangle(5, 5);
+            Shape tri = new Triangle(5, 5);
+
+            Console.WriteLine("Rect Area" + rect.area());
+            Console.WriteLine("Tri Area" + tri.area());
+
+            Rectangle combRect = new Rectangle(5, 5) + new Rectangle(5, 5);
+
+            Console.WriteLine("combRect Area " + combRect.area());
+
+        }
+
+    }
+    
+    class Dog : Animal
+    {
+        public string favFood { get; set;  }
+
+        public Dog() : base()
+        {
+            this.favFood = "No Favorite Food";
+        }
+
+        public Dog(double height, double weight, string name, string sound, string favFood) : base(height, weight, name, sound)
+        {
+            this.favFood = favFood;
+        }
+
+        new public string toString()
+        {
+            return String.Format("{0} is {1} inches tall, weighs {2} lbs and likes to say {3} and eats {4}", name, height, weight, sound favFood);
+        }
+
+    }
+
+    abstract class Shape
+    {
+        public abstract double area();
+
+        public void sayHi()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
+    public interface ShapeItem
+    {
+        double area();
+    }
+     
+    class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double num1, double num2)
+        {
+            length = num1;
+            width = num2;
+        }
+
+        public override double area()
+        {
+            return length * width;
+        }
+
+        public static Rectangle operator + (Rectangle rect1, Rectangle rect2)
+        {
+            double rectLength = rect1.length + rect2.length;
+            double rectWdith = rect1.width + rect2.width;
+
+            return new Rectangle(rectLength, rectWidth);
+        }
+    }
+
+    class Triangle : Shape
+    {
+        private double theBase;
+        private double height;
+
+        public Triangle(double num1, double num2)
+        {
+            theBase = num1;
+            height = num2;
+        }
+
+        public override double area()
+        {
+            return .5 * (theBase * height);
+        }
+    }
+         
+}
+
+
