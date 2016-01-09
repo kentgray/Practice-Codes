4e903b92553fcbc4ff6831a3fdd1495d4e065512
Kent Gray <aggie12man@gmail.com>
2016-01-08 16:54:52 -0800

    C# Anonymous Expressions
    C# Tutorial

---
+++ C#/Programs/Succinctly Codes/Anonymous Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate double GetSum(double num1, double num2);
+
+namespace Anonymous_Methods
+{
+    class Animal
+    {
+        static void Main(string[]args)
+        { 
+            GetSum sum = delegate (double num1, double num2)
+        {
+            return num1 + num2;
+        };
+
+            Console.WriteLine("5 + 10 = " + sum(5, 10));
+        }
+    }
+}
